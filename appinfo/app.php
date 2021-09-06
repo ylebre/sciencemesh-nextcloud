0aaf00e65ee4013377133a78d8a223a64a2d88fa
Michiel de Jong <michiel@unhosted.org>
2021-09-06 13:19:21 +0200

    moved to https://github.com/pondersource/nc-sciencemesh

--- appinfo/app.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	declare(strict_types=1);
-
-	$app = \OC::$server->query(\OCA\ScienceMesh\AppInfo\ScienceMeshApp::class);
--- templates/navigation/index.php
+++
@@ -1,3 +0,0 @@
-<ul class="with-icon">
-    <li><a class="icon-star-dark" href="#">ScienceMesh</a></li>
-</ul>
